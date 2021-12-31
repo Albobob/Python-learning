5ea28d97289c0808c117c820d479c0a098aa9f46
albobob <aboolcast@gmail.com>
2021-12-31 21:55:48 +0300

    task_4(for i)

--- task_4.py
+++ task_4.py
@@ -4,14 +4,25 @@
 Для решения используйте цикл while и арифметические операции.
 '''
 
+# n = abs(int(input("Введите целое положительное число ")))
+# max = n % 10
+# while n >= 1:
+#     n = n // 10
+#     if n % 10 > max:
+#         max = n % 10
+#     if n > 9:
+#         continue
+#     else:
+#         print("Максимальное цифра в числе ", max)
+#         break
+
+# Вариант через цикл
+
 n = abs(int(input("Введите целое положительное число ")))
-max = n % 10
-while n >= 1:
-    n = n // 10
-    if n % 10 > max:
-        max = n % 10
-    if n > 9:
-        continue
-    else:
-        print("Максимальное цифра в числе ", max)
-        break
+
+ls = []
+
+for i in str(n):
+    ls.append(int(i))
+
+print(max(ls))
